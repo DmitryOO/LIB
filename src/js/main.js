f805ccb53473f3a9d44b78c881284d283e20c200
Dmitry <dimkut@mail.ru>
2022-07-28 21:48:39 +0400

    elemMethods(part1)

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 import $ from './lib/lib';
 
 $('button').click(function(){
-  $(this).toggleClass('active');
+  $('div').eq(2).toggleClass('active');
 });
+$('div').click(function(){
+  console.log($(this).index());
+});
+console.log($('div').eq(2).find('.more'));
