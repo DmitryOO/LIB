311a4669c0e80ef87798f533fdfd41ab9fe3eecb
Dmitry <dimkut@mail.ru>
2022-08-18 20:36:18 +0400

    styles+buttons components

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,21 @@
 import $ from './lib/lib';
 
-$('button').click(function(){
-  $('div').eq(2).toggleClass('active');
-});
-$('div').click(function(){
-  console.log($(this).index());
-});
+// $('button').click(function(){
+//   $('div').eq(2).toggleClass('active');
+// });
+// $('div').click(function(){
+//   console.log($(this).index());
+// });
 // console.log($('div').eq(2).find('.more'));
 // console.log($('.some').closest('.findme'));
-$('button').fadeIn(5000,);
+// $('button').fadeIn(5000,);
 
+$('#first').click(()=>{
+  $('div').eq(1).fadeOut(500);
+});
+$('[data-count="second"]').click(()=>{
+  $('div').eq(2).fadeOut(500);
+});
+$('button').eq(2).on('click', ()=> {
+  $('.w-500').fadeOut(800);
+});
