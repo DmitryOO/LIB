96000a6e65a4b0122a515f91b1b6c44db65b13b8
Dmitry <dimkut@mail.ru>
2022-12-31 13:22:10 +0400

    modal dynamic

--- src/js/main.js
+++ src/js/main.js
@@ -32,3 +32,36 @@ $('.wrap').html(`<div class="dropdown ">
 </div>
 </div>`);
 $('.dropdown-toggle').dropdown(); 
+
+$('#trigger').click(()=>$('#trigger').createModal({
+  text: {
+    title: 'ПиПиПи',
+    body: '           Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum minus doloremque nesciunt enim rem quam corporis? Dolorem pariatur magnam distinctio perferendis. Ratione dolorem voluptates iusto facilis odit   veritatis, suscipit voluptatibus!'
+  },
+  btns: {
+    count:3,
+    settings: [
+      [
+        'Close',
+        ['btn-danger','mr-10'],
+        true,
+      ],
+      [
+        'Save changes',
+        ['btn-success'],
+        false,
+        ()=> {
+          alert('данные сохранены');
+        }
+      ],
+      [
+        'some button',
+        ['btn-warning', 'ml-10'],
+        false,
+        ()=> {
+          prompt('данные сохранены', ',kz');
+        }
+      ]
+    ]
+  }
+}));
